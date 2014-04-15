9ce2b8873c06517a77c18e11ef0987b7eeb1e988
Benny <ffx@ukr.net>
2014-04-16 02:42:56 +0300

    plugin modified, now it sync with actual channel list

--- epg-generator/plugin/EPG.sh
+++ epg-generator/plugin/EPG.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
-tmp_dir="/tmp"
 mem_limit="100"
+sch_str="epg_dir"
 file_url="http://epg_source/"
 file_name="epg.tar.gz"
 file_conf="/var/tuxbox/config/neutrino.conf"
 
+if [ -d /tmp ]
+then
+tmp_dir="/tmp"
+fi
 
-echo `cat $file_conf | grep epg_dir` > $tmp_dir/conf
+if [ -d /temp ]
+then
+tmp_dir="/temp"
+fi
+
+echo `cat $file_conf | grep $sch_str` > $tmp_dir/conf
 source $tmp_dir/conf
 psize=${#epg_dir}
 
 if [ $psize -eq 0 ]
 then
 echo "EPG path variable not set or not found, aborting!"
-exit 1
+exit
 fi
 
 if [ ! -d $epg_dir ]
 then
 echo "EPG path not found, aborting!"
-exit 1
+exit
 fi
 
 free_mem=`df -m $epg_dir | tail -1 | awk '{print $4}'`
@@ -28,7 +37,7 @@ free_mem=`df -m $epg_dir | tail -1 | awk '{print $4}'`
 if [ $free_mem -lt $mem_limit ]
 then
 echo "Not enough space for EPG files, aborting!"
-exit 1
+exit
 fi
 
 echo "Getting new file from server..."
@@ -37,26 +46,51 @@ wget -qP $tmp_dir/ $file_url$file_name
 if [ ! -f $tmp_dir/$file_name ]
 then
 echo "EPG data file not found, aborting!"
-exit 1
+exit
 fi
 
-if [ ! -d $epg_dir/$tmp_dir ]
+if [ ! -d $epg_dir$tmp_dir ]
 then
-mkdir $epg_dir/$tmp_dir
+mkdir $epg_dir$tmp_dir
 fi
 
 echo "Removing old files..."
 rm $epg_dir/*.*
-rm $epg_dir/$tmp_dir/*.*
+rm $epg_dir$tmp_dir/*.*
 
 echo "Unpacking..."
-cd $epg_dir/$tmp_dir/
+cd $epg_dir
 tar -xzf $tmp_dir/$file_name
-
 rm $tmp_dir/$file_name
 
+echo "Getting channels list..."
+bq=`pzapit | grep ther | cut -c1`
+wget -q -O $tmp_dir/channels http://127.0.0.1/y/cgi?execute=func:get_channels_as_dropdown%20$bq
+
+for f in `cut -c19-30 $tmp_dir/channels`; do
+echo $f".xml" >> $tmp_dir/files 
+done
+
+for f in `cat $tmp_dir/files`; do
+echo "Moving actual data..."
+mv $epg_dir/$f $epg_dir/$tmp_dir > /dev/null 2>&1
+done
+
+echo "Generating index file..."
+cd $epg_dir$tmp_dir
+ls -1 | tr '\n' '\0' | xargs -0 -n 1 basename > $tmp_dir/files
+echo '<?xml version="1.0" encoding="UTF-8"?>' > index.xml
+echo "<dvbepgfiles>" >> index.xml
+for f in `cat $tmp_dir/files`; do
+echo "<eventfile name=\""$f"\"/>" >> index.xml
+done
+echo "</dvbepgfiles>" >> index.xml
+
+echo "Clean up..."
+rm $epg_dir/*.*
+
 echo "Start reloading EPG data, this can take some time..."
-sectionsdcontrol --readepg $epg_dir/$tmp_dir
+sectionsdcontrol --readepg $epg_dir$tmp_dir
 
 echo "Done!"
-exit 0
+exit
